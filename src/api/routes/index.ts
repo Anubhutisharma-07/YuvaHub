09dff072687fddf7293ded749c571c3f9462277b
Milan <milanrathod5201@gmail.com>
2026-08-29 17:40:45 +0530

    feat: implement native WebRTC signaling server and synchronized real-time workspace canvas

--- src/api/routes/index.ts
+++ src/api/routes/index.ts
@@ -39,6 +39,7 @@ import testimonialRoutes from "./testimonialRoutes.js";
 import eventRsvpRoutes from "./eventRsvpRoutes.js";
 import skillAssessmentRoutes from "./skillAssessmentRoutes.js";
 import voteRoutes from "./voteRoutes.js";
+import studyRoomRoutes from "./studyRoomRoutes.js";
 import { errorHandler } from "../middlewares/errorHandler.js";
 import { apiVersionHeaders } from "../versioning/middleware.js";
 
@@ -87,6 +88,7 @@ const routes = [
   eventRsvpRoutes,
   skillAssessmentRoutes,
   voteRoutes,
+  studyRoomRoutes,
 ];
 
 // Mount all routes onto v1Router
